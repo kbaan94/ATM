109143cd5d585d859bb088ea7ef831f2008a6a58
Khaled <khaledban@hotmail.com>
2020-08-03 06:11:33 -0400

    updating user and account classes

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,21 @@
+import java.util.ArrayList;
 
 public class Account {
+	
+	//Account name
+	private String name;
+	
+	//Every account has a ID different from the User ID
+	private String uuid;
+	
+	//Current balance of the account
+	private double balance;
+	
+	//Some user owns this account, links to that User. 
+	//...The User Object that owns this account
+	private User holder;
+	
+	//List of transactions for this account
+	private ArrayList<Transaction> transactions;
 
 }
--- src/User.java
+++ src/User.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
 
 public class User {
-
-	public static void main(String[] args) {
-		System.out.println("Hello ATM");
-	}
+	
+	//First name of user
+	private String name;
+	
+	//Last name of user
+	private String lastName;
+	
+	//ID of user
+	private String uuid;
+	
+	//The MD5 hash of the user
+	private byte pinHash[];
+	
+	//The list of accounts the user holds
+	private ArrayList<Account> accounts;
 
 }
